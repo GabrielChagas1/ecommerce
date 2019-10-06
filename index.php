b825949a6399292c58540ed1b104c7dee9300963
Gabriel Chagas <gabrielserq@outlook.com>
2019-10-06 02:08:10 -0300

    arrumando layout admin

--- index.php
+++ index.php
@@ -4,16 +4,24 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//route para o site
 $app->get('/', function() {
   
    $page = new Page();
    $page->setTpl("index");
+});
 
+//route para o admin
+$app->get('/admin', function() {
+  
+   $page = new PageAdmin();
+   $page->setTpl("index");
 });
 
 $app->run();
