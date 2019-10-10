4cbee04bfb5c34a0fb362defd23315ab898db38b
Gabriel Chagas <gabrielserq@outlook.com>
2019-10-10 17:43:15 -0300

    update das categorias

--- index.php
+++ index.php
@@ -132,13 +132,34 @@ $app->post("/admin/categories/create", function(){
    exit;
 });
 
+//deletar categorias
 $app->get("/admin/categories/:idcategory/delete", function($idcategory){
+   User::verifyLogin();
    $category = new Category();
    $category->get((int)$idcategory);
    $category->delete();
    header('Location: /admin/categories');
    exit;
 });
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+   User::verifyLogin();
+   $category = new Category();
+   $category->get((int) $idcategory);
+   $page = new PageAdmin();
+   $page->setTpl("categories-update", array("category" =>  $category->getValues()));
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+   User::verifyLogin();
+   $category = new Category();
+   $category->get((int) $idcategory);
+   $category->setData($_POST);
+   $category->save();
+   header('Location: /admin/categories');
+   exit;
+});
+
 
 
 $app->run();
